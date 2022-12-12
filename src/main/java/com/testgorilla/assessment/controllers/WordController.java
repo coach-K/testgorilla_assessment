7c68d463d53215b4db7d1d29e3526af59488bd5c
Kehinde <kehinde@incentivegamesltd.com>
2022-12-12 18:51:02 +0100

    Completed task-6

--- src/main/java/com/testgorilla/assessment/controllers/WordController.java
+++ src/main/java/com/testgorilla/assessment/controllers/WordController.java
@@ -3,6 +3,8 @@ package com.testgorilla.assessment.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.testgorilla.assessment.models.CustomError;
 import com.testgorilla.assessment.models.Word;
 import com.testgorilla.assessment.services.WordService;
 
@@ -25,13 +28,19 @@ public class WordController {
     }
 
     @PostMapping
-    public Word postWord(@RequestBody Word word) {
+    public ResponseEntity postWord(@RequestBody Word word) throws Exception {
+        //Check if string match regex a-zA-Z
+        if (!word.getWord1().matches("[a-zA-Z\\s]*") 
+        || !word.getWord2().matches("[a-zA-Z\\s]*")
+        || !word.getRelation().matches("[a-zA-Z\\s]*")) {
+            return new ResponseEntity<>(new CustomError("Words cannot contain special characters or numeric"), HttpStatus.BAD_REQUEST);
+        }
         Word newWord = new Word(
             word.getWord1().toLowerCase().trim(), 
             word.getWord2().toLowerCase().trim(), 
             word.getRelation().toLowerCase().trim()
             );
-        return this.service.save(newWord);
+        return new ResponseEntity<>(this.service.save(newWord), HttpStatus.OK);
     }
 
     @GetMapping
---
+++ src/main/java/com/testgorilla/assessment/models/CustomError.java
@@ -0,0 +1,13 @@
+package com.testgorilla.assessment.models;
+
+public class CustomError {
+    private String message;
+
+    public CustomError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
